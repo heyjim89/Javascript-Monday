37ee69ccf1e9312c5d0833d1d3051579a270153e
Jim D <heyjim89@gmail.com>
2015-06-22 09:37:06 -0600

    Added alerts and strings

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 // Variable Declarations
 var result = 10 / 3;
 alert(result);
+
+var greeting = 'Hello' + 'World';
+
+var eleven = '1'+'1';
+
+// alert(eleven);
+// 
+// Properties
+// 
+// Strings have properties
+// Bracket Notation
+
+
+// dot-notation. Sometimes doesn't work
+// alert(greeting.length);
+
+
+// Strings are zereo indexed 
+// 
